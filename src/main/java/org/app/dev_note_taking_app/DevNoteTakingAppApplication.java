0535b1d5dea91214616503019bc1cf95e9b0eda7
Prabhashperera <prabashmperera01@gmail.com>
2025-07-29 20:02:40 +0530

    P INIT

---
+++ src/main/java/org/app/dev_note_taking_app/DevNoteTakingAppApplication.java
@@ -0,0 +1,13 @@
+package org.app.dev_note_taking_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevNoteTakingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevNoteTakingAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/app/dev_note_taking_app/DevNoteTakingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.app.dev_note_taking_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevNoteTakingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
